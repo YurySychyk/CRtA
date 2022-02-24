432eefac51316d05b1993dcd5f13218ca3091b28
Yury Sychyk <kauz@tut.by>
2022-02-24 11:31:55 +0300

    change logic

--- Converter-roman-numerals-to-arabic/Logic.swift
+++ Converter-roman-numerals-to-arabic/Logic.swift
@@ -8,73 +8,70 @@
 import Foundation
 var number = 0
 var symbols = [Character]()
-var cor = 0
 func romanNumbers(roman: [Character]) -> Int {
     number = 0
+    var cor = 0
   
     for i in 0...symbols.count {
-        if (i + cor + 1) <= symbols.count {
-            let a = symbols[i + cor]
-            var b: Character = "q"
-            if symbols.count == 1 {
-                b = "q"
-            } else if symbols.count == 2 {
-                b = symbols[1]
-            } else {
+        var a: Character
+        var b: Character
+        switch symbols.count {
+        case 0 :
+            a = " "
+            b = " "
+        case 1 :
+            a = symbols[0]
+            b = " "
+        default :
+            if i + 1 + cor <= symbols.count {
+                a = symbols[i + cor]
                 b = symbols[i + 1 + cor]
+            } else {
+                a = symbols[i + cor]
+                b = " "
             }
-        switch (a, b){
+           
+        }
+        
+        switch (a, b) {
         case ("I","V"):
             number += 4
             cor += 1
-           // print(number)
         case ("I", "X"):
             number += 9
             cor += 1
-           // print(number)
         case ("I",_):
             number += 1
-           /// print(number)
         case ("V",_):
             number += 5
-           // print(number)
         case ("X", "L"):
             number += 40
             cor += 1
-           // print(number)
         case ("X", "C"):
             number += 90
             cor += 1
-          //  print(number)
         case ("X",_):
             number += 10
-          //  print(number)//
         case ("L",_):
             number += 50
-          //  print(number)
         case ("C", "D"):
             number += 400
             cor += 1
-          //  print(number)
         case ("C", "M"):
             number += 900
             cor += 1
-          //  print(number)
         case ("C",_):
             number += 100
-          //  print(number)
         case ("D",_):
             number += 500
-            print(number)
         case ("M",_):
             number += 1000
-          //  print(number)
         default:
             return 0
         }
     }
-    }
-   // print(number)
     return number
-}
-//romanNumbers(roman: "MCMXCIV")
+    }
+  
+
+
